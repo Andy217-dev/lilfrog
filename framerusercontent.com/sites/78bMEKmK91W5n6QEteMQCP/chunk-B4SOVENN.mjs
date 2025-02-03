46ff1f6a2ff1caca8247b5b6cc56c70d777c293e
Andy217-dev <andcandy42@gmail.com>
2025-02-03 13:51:49 -0500

    new

--- framerusercontent.com/sites/78bMEKmK91W5n6QEteMQCP/chunk-B4SOVENN.mjs
+++ framerusercontent.com/sites/78bMEKmK91W5n6QEteMQCP/chunk-B4SOVENN.mjs
@@ -1,2 +1,2 @@
-function a(t,e){return{description:"Lil Frog the trippy frog-man from a magical swamp, living off psychedelic mushrooms and growing 12 inches of green on demand. Inspired by Matt Furie\u2019s Hedz, this meme project dives into fart introspection, wild humor, and magical mayhem. Ribbit!",robots:"max-image-preview:large",title:"Ribbit. Ribbit. Ribbit."}}export{a};
+function a(t,e){return{description:"Lil Frog the trippy frog-man from a magical swamp, living off psychedelic mushrooms and growing 12 inches of green on demand. Inspired by Matt Furie\u2019s Hedz, this meme project dives into fart introspection, wild humor, and magical mayhem. Ribbit!",robots:"max-image-preview:large",title:"Lil Frog."}}export{a};
 //# sourceMappingURL=chunk-B4SOVENN.mjs.map
